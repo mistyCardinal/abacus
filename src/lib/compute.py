3e34b2a254224004006ce40acfa55dd9908fb5b7
mistyCardinal <cardinal@mistrise.com>
2019-12-02 21:37:08 -0500

    Add a class for math functions - correction

--- src/lib/compute.py
+++ src/lib/compute.py
@@ -1,3 +1,18 @@
 # Exercise 1
 
 class Compute:
+	def _init_(self, operator operands):
+		self.operator = operator
+		self.operands = operands
+		
+	def add(self):
+		pass
+		
+	def subtrace(self):
+		pass
+		
+	def divide(self):
+		pass
+		
+	def multiply(self):
+		pass
