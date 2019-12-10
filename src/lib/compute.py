effc20dda2caf5e4a00feb9ae0a01fd3526cbad3
mistyCardinal <cardinal@mistrise.com>
2019-12-10 15:43:29 -0500

    Add puff

--- src/lib/compute.py
+++ src/lib/compute.py
@@ -6,7 +6,7 @@ class Compute:
 		self.operands = operands
 		
 	def add(self):
-		addition = 0
+		tryagain = 0 # stuff 3
 		
 	def subtract(self):
 		difference = 0
@@ -18,13 +18,13 @@ class Compute:
 		pass
 		
 	def multiply(self):
-	   product = 0
+	   product = 0 
        if self.operands is None
           return
        print(product)
 		
-	#another random change ft2
+	#another random change ft2 3
 	
-	# change this
+	# change this 3
 	
-	# and also now ft3
+	# and also now ft3 3
